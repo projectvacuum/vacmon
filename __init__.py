36c7715739d2e83a1dec50c8f2a823e32ebdf4a6
Andrew McNab <andrew.mcnab@cern.ch>
2018-06-22 10:59:59 +0100

    Highlight factories absent for > 1 hour

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 #  __init__.py for vacmon package
 #
 #  Andrew McNab, University of Manchester.
-#  Copyright (c) 2015-6. All rights reserved.
+#  Copyright (c) 2015-7. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or
 #  without modification, are permitted provided that the following
